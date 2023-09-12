288af7ccc23c109d12bf56924cf3f435c7308451
PAAG <keapaagdat23a@gmail.com>
2023-09-12 15:48:11 +0200

    Opgave4a.java added

---
+++ src/Opgave4a.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class Opgave4a {
+  int[] numbersA = new int[10];
+  int[] numbersB = new int[10];
+
+  public static void main(String[] args) {
+    new Opgave4a().run();
+  }
+
+  void initArrays() {
+    for (int i = 0; i < numbersA.length; i++) {
+      numbersA[i] = 1;
+      numbersB[i] = 2;
+    }
+  }
+
+  private void run() {
+    initArrays();
+    printArrays("FIRST ROUND", numbersA, numbersB);
+
+    // Uncomment ONE of the following lines at a time:
+    // numbersB = numbersA;
+    // numbersB = numbersA.clone();
+    // numbersB = Arrays.copyOf(numbersA, numbersB.length);
+    // System.arraycopy(numbersA, 0, numbersB, 0, numbersA.length);
+
+    numbersA[5] = 3;
+    numbersB[5] = 4;
+
+    printArrays("SECOND ROUND", numbersA, numbersB);
+  }
+
+  private void printArrays(String s, int[] a, int[] b) {
+    System.out.println(s);
+    for (int i = 0; i < a.length; i++) {
+      System.out.println(a[i]);
+    }
+    for (int i = 0; i < b.length; i++) {
+      System.out.println(b[i]);
+    }
+  }
+}
